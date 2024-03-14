6c9fb780831b3c41a41c69f932c43180cf72780a
Valya <V2lentin2005@yandex.ru>
2024-03-14 19:45:02 +0300

    alittle bit changes in 83 string

--- oop.h
+++ oop.h
@@ -1,72 +1,85 @@
-#include"includes.h"
+#include "includes.h"
 
 // Делегирующий конструктор
-struct S {
-  S(int a) {
+struct S
+{
+  S(int a)
+  {
   }
 
-  S(int a, size_t c): S(a) {
+  S(int a, size_t c) : S(a)
+  {
   }
 };
 
-//Можно явно попросить компилятор сгенерировать метод  если он его умеет генерировать (конструктор по умолчанию, конструктор копирования, оператор присваивания, деструктор и тд).
+// Можно явно попросить компилятор сгенерировать метод  если он его умеет генерировать (конструктор по умолчанию, конструктор копирования, оператор присваивания, деструктор и тд).
 
-struct S {
+struct S
+{
   S(int a) {}
   S() = default;
 };
 
-//Можно запретить компилятору генерировать метод
+// Можно запретить компилятору генерировать метод
 
-struct S {
-  S(const S&) = delete;
+struct S
+{
+  S(const S &) = delete;
 };
 
 // дружественные функции
 
-class C {
+class C
+{
   int a = 0;
   friend void foo(int);
   friend class D;
 };
 
-void foo(int b) {
+void foo(int b)
+{
   C c;
   std::cout << c.a + b; // c.a это OK
 }
 
-class D {};
+class D
+{
+};
 
 // перегрузка оператора []
-
-class Vector {
+template <class T>
+class Vector
+{
 public:
-  int& operator[](size_t index) { return arr_[index]; }
-  const int& operator[](size_t index) const {return arr_[index]; }
+  int &operator[](size_t index) { return arr_[index]; }
+  const int &operator[](size_t index) const { return arr_[index]; }
+
 private:
-  int* arr_ = nullptr;
+  int *arr_ = nullptr;
   size_t size_ = 0;
 };
 
-template<typename T>
-T GetMax(const T& a, const T& b) {
-    return a > b ? a : b;
+template <typename T>
+T GetMax(const T &a, const T &b)
+{
+  return a > b ? a : b;
 }
 template <typename T>
-class vector {
-  private:
-    T* arr_ = nullptr;
+class vector
+{
+private:
+  T *arr_ = nullptr;
 };
 
 template <typename T>
-struct Vector {
-    template <typename U>
-    void push_back(const U&);
+struct Vector1
+{
+  template <typename U>
+  void push_back(const U&);
 };
 
 template <typename T>
 template <typename U>
-void Vector<T>::push_back(const U&) {}
+void Vector1<T>::push_back(const U&) {}
 
-
-
+int main(){}
